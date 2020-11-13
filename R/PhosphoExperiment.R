f006e9598d92b79b637911e47f171cd9399323b1
Taiyun Kim <taiyun.kim91@gmail.com>
2020-11-13 13:12:51 +1100

    Updated PhosphoExperiment class

--- R/PhosphoExperiment.R
+++ R/PhosphoExperiment.R
@@ -1,120 +1,129 @@
-### =========================================================================
-### PhosphoExperiment object
-### -------------------------------------------------------------------------
-###
 #' @export
-#' @importFrom utils packageVersion
+#' @rdname PhosphoExperiment
 #' @importFrom S4Vectors SimpleList
-#' @importFrom stats setNames
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @importClassesFrom S4Vectors DataFrame SimpleList
-#' 
-setClass("PhosphoExperiment",
-         slots=list(Quantification="matrix", 
-                    UniprotID="character", 
-                    GeneSymbol="character", 
-                    Site="numeric", 
-                    Residue="character", 
-                    Sequence="character")
+setClass("PhosphoExperiment", 
+    slots=list(
+        UniprotID="character", 
+        GeneSymbol="character", 
+        Site="numeric", 
+        Residue="character", 
+        Sequence="character"
+    ),
+    contains = "SummarizedExperiment"
+    
 )
+
+
+
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### Getters/setters for internal slots
 ###
-#' @export
 setGeneric("Quantification", function(x, type, ...) standardGeneric("Quantification"))
 
-#' @export
 setGeneric("Quantification<-", function(x, type, ..., value) standardGeneric("Quantification<-"))
 
-#' @export
 setGeneric("UniprotID", function(x) standardGeneric("UniprotID"))
 
-#' @export
 setGeneric("UniprotID<-", function(x, value) standardGeneric("UniprotID<-"))
 
-#' @export
 setGeneric("GeneSymbol", function(x, ...) standardGeneric("GeneSymbol"))
 
-#' @export
 setGeneric("GeneSymbol<-", function(x, value) standardGeneric("GeneSymbol<-"))
 
-#' @export
 setGeneric("Site", function(x, ...) standardGeneric("Site"))
 
-#' @export
 setGeneric("Site<-", function(x, value) standardGeneric("Site<-"))
 
-#' @export
 setGeneric("Residue", function(x, ...) standardGeneric("Residue"))
 
-#' @export
 setGeneric("Residue<-", function(x, value) standardGeneric("Residue<-"))
 
-#' @export
 setGeneric("Sequence", function(x, ...) standardGeneric("Sequence"))
 
-#' @export
 setGeneric("Sequence<-", function(x, value) standardGeneric("Sequence<-"))
 
-#' @export
 
 setReplaceMethod("UniprotID", signature="PhosphoExperiment", function(x, value) {
-        x@UniprotID <- as.character(value)
-        return(x)
+    x@UniprotID <- as.character(value)
+    return(x)
 })
 
-#' @export
 
 setReplaceMethod("GeneSymbol", signature="PhosphoExperiment", function(x, value) {
     x@GeneSymbol<- as.character(value)
     return(x)
 })
 
-#' @export
 
 setReplaceMethod("Site", signature="PhosphoExperiment", function(x, value) {
     x@Site <- as.numeric(value)
     return(x)
 })
 
-#' @export
 
 setReplaceMethod("Residue", signature="PhosphoExperiment", function(x, value) {
     x@Residue <- as.character(value)
     return(x)
 })
 
-#' @export
 
 setReplaceMethod("Sequence", signature="PhosphoExperiment", function(x, value) {
     x@Sequence<- as.character(value)
     return(x)
 })
 
+
+
+
+
+
+
+#'The PhosphoExperiment class
+#'
+#'
+#'
+#' @docType class
+#' @aliases 
+#' coerce, SummarizedExperiment, PhosphoExperiment-method
+#' @export
+#' @importFrom S4Vectors SimpleList
+#' @importFrom methods is as
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#'
 PhosphoExperiment <- function(..., UniprotID=c(), GeneSymbol=c(), Site=c(), Residue=c(), Sequence=c()) {
+    se <- SummarizedExperiment(...)
+    .se_to_pe(
+        se,
+        UniprotID,
+        GeneSymbol,
+        Site,
+        Residue,
+        Sequence
+    )
+}
+
+#' @importFrom S4Vectors DataFrame SimpleList
+#' @importClassesFrom S4Vectors DataFrame
+#' @importFrom methods new
+.se_to_pe = function(se, UniprotID=c(), GeneSymbol=c(), Site=c(), Residue=c(), Sequence=c()) {
     
-    out <- new("PhosphoExperiment", 
-               Quantification=...
-               )
     
+    
+    out <- new("PhosphoExperiment", se)
     UniprotID(out) <- UniprotID
     GeneSymbol(out) <- GeneSymbol
     Site(out) <- Site
     Residue(out) <- Residue
     Sequence(out) <- Sequence
     
+    
+    
     out
 }
 
-#library(PhosR)
-#quant <- as.matrix(PhosR::phospho.L6.ratio)
-#uniprot <- as.character(sapply(strsplit(rownames(quant),"~"), function(x) x[[2]]))
-#symbol <- as.character(sapply(strsplit(rownames(quant),"~"), function(x) x[[2]]))
-#site <- as.numeric(gsub("[STY]","",sapply(strsplit(rownames(quant),"~"), function(x) x[[3]])))
-#res <- as.character(gsub("[0-9]","",sapply(strsplit(rownames(quant),"~"), function(x) x[[3]])))
-#seq <- as.character(sapply(strsplit(rownames(quant),"~"), function(x) x[[4]]))
-#
-#phosData <- PhosphoExperiment(Quantification = quant, UniprotID = uniprot)
-#phosData <- PhosphoExperiment(Quantification = quant, Site= site)
 
 
