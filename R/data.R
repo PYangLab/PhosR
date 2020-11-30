d1ae44c35392d023f7939cf883ff948d5978a876
DiXiao3447 <dxia3447@uni.sydney.edu.au>
2020-11-30 15:26:45 +1100

    minor change

--- R/data.R
+++ R/data.R
@@ -122,7 +122,7 @@
 #' @description L6 myotube phosphoproteome dataset
 #' (accession number: PXD019127).
 #'
-#' @usage data(phospho_L6_ratio.pe)
+#' @usage data(phospho.L6.ratio.pe)
 #'
 #' @source PRIDE accesion number: PXD001792
 #'
