7d61f5ccda8a52426dcde63a16791c8b23e1e885
hani <hani.kim127@gmail.com>
2020-12-05 12:52:13 +1100

    update getSPS

--- R/getSPS.R
+++ R/getSPS.R
@@ -1,29 +1,43 @@
-
-## format of input dataset
-# 1. PhosphoExperiment objects
-# 2. Quantification matrix in PhosphoExperiment object is log2 fold change matrix without control
-
-## parameter
-# phosData: a list of users' PhosphoExperiment objects from which generate SPSs
-# conds: a numeric vector contains the numbers of conditions in each of the datasets
-# num: the number of identified SPSs, by default is 100
-# residuleInfo: whether the phosphosite contains amino acid information or not, by default is FALSE
-
+#' Generate set of stable phosphoporylated sites
+#'
+#' @usage getSPS(phosData, conds)
+#'
+#' @param phosData a list of users' PhosphoExperiment objects from which generate SPSs
+#' @param conds a list of vector contains the conditions labels for each sample in the phosphoExperiment objects
+#' @param num the number of identified SPSs, by default is 100
+#' @param residueInfo whether the phosphosite contains amino acid information or not, by default is FALSE
+#'
+#' @return A vectors of stably phosphorylated sites
+#'
+#' @examples
+#'
+#' data("phospho_L6_ratio_pe")
+#' data("phospho.liver.Ins.TC.ratio.RUV.pe")
+#' 
+#' ppe1 <- phospho.L6.ratio.pe
+#' ppe2 <- phospho.liver.Ins.TC.ratio.RUV.pe
+#' 
+#' ppe.list <- list(ppe1, ppe2)
+#' cond.list <- list(grp1 = gsub("_.+", "", colnames(ppe1)),
+#'                   grp2 = str_sub(colnames(ppe2), end=-5))
+#' 
+#' inhouse_SPSs <- getSPS(ppe.list, conds = cond.list, residueInfo = F)
+#' 
+#' @export
+#' 
 getSPS <- function(phosData = ..., conds = ..., num = 100, residueInfo = FALSE) {
-    sites <- list()
-    sites.unique <- list()
-    mat.max <- list()
+    sites <- sites.unique <- mat.max <- list()
     n <- length(phosData)
     m <- length(conds)
     if (n < 2) {
         stop("Please use more than one dataset")
     }
     if (n != m) {
-        stop("Please use same number of datasets and conditions")
+        stop("Please use the same number of datasets and conditions")
     }
     for (i in 1:n) {
         if (!"PhosphoExperiment" %in% is(phosData[[i]])) {
-            stop("Wrong phosData, need to be PhosphoExperiment object") }
+            stop("Wrong phosData, need to be a PhosphoExperiment object") }
         }
     for (i in 1:n) {
         if (residueInfo) {
@@ -33,21 +47,21 @@ getSPS <- function(phosData = ..., conds = ..., num = 100, residueInfo = FALSE) 
             sites[[i]] <- paste(toupper(phosData[[i]]@GeneSymbol), phosData[[i]]@Site, sep = ";")
         }
         sites.unique[[i]] <- unique(sites[[i]])
-        nrep <- ncol(phosData[[i]]@data$Quantification)/conds[i]
+        nrep <- ncol(phosData[[i]]@assays@data$Quantification)/length(unique(conds[[i]]))
         if (nrep == 1) {
-            mat.mean <- phosData[[i]]@data$Quantification # if no replicates
+            mat.mean <- phosData[[i]]@assays@data$Quantification # if no replicates
         } else {
-            grps <- rep(seq_len(conds[i]), each = nrep)
-            mat.mean <- PhosR::meanAbundance(phosData[[i]]@data$Quantification, grps)
+            grps <- conds[[i]]
+            mat.mean <- PhosR::meanAbundance(phosData[[i]]@assays@data$Quantification, grps)
         }
         
-        sites.mean <- t(sapply(split(data.frame(mat.mean), sites[[i]]), colMeans))
+        sites.mean <- t(sapply(split(as.data.frame(mat.mean), sites[[i]]), colMeans))
         # maximum fold change for each phosphosite
         sites.max <- apply(sites.mean, 1, function(x){x[which.max(abs(x))]})
         mat.max[[i]] <- sites.max
     }
     
-    # identify the overlapped sites in all datasets, if it is less than 1000, find top 1000 overlapped phosphosites
+    # identify the overlapped sites in all datasets, if it is more than 1000, find top 1000 overlapped phosphosites
     o <- as.data.frame(table(unlist(sites.unique)))
     if (length(o$Freq == m) > 1000) {
         top <- as.character(o[which(o$Freq == m), 1])
